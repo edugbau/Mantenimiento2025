18e17f769d9e960bab0787113c1caf7b55a0db7d
edugbau <edugb@uma.es>
2025-03-06 17:24:48 +0100

    Terminado gran parte de las pruebas de clubdeportivo

--- Practica1/src/test/java/clubDeportivo/clubDeportivoTest.java
+++ Practica1/src/test/java/clubDeportivo/clubDeportivoTest.java
@@ -8,9 +8,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class clubDeportivoTest {
     ClubDeportivo club;
+    Grupo grupo;
     @BeforeEach
     void setUp() throws ClubException {
         club = new ClubDeportivo("Sons",3);
+        String codigo="Futbol";
+        String actividad="Futbol";
+        int nplazas=20;
+        int nmatriculados=10;
+        int tarifa=50;
+        try {
+            grupo = new Grupo(codigo, actividad, nplazas, nmatriculados, tarifa);
+        } catch (ClubException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
@@ -35,17 +46,55 @@ public class clubDeportivoTest {
     }
 
     @Test
-    @DisplayName("Añadir grupo nuevo")
-    void anyadirGrupoNuevo() throws ClubException {
-        //Arrange
-        String[] datos = {"Samcro", "Motociclismo", "2", "8", "1000"};
-        // String[] datos1 = {"Sambel", "Motociclismo", "5", "5", "1500"};
+    @DisplayName("Añadir grupo no existente")
+    void anyadirGrupoNoExistente() throws ClubException{
+        assertDoesNotThrow(() -> club.anyadirActividad(grupo));
+    }
+    @Test
+    @DisplayName("Añadir grupo existente")
+    void anyadirGrupoExistente() throws ClubException{
+        club.anyadirActividad(grupo);
+        assertDoesNotThrow(() -> club.anyadirActividad(grupo));
+    }
 
-        //Act
-        club.anyadirActividad(datos);
-        //club.anyadirActividad(datos1);
+    @Test
+    @DisplayName("Añadir grupo mediante datos")
+    void anyadirGrupoMedianteDatos() throws ClubException{
+        Integer matriculados = 10;
+        Integer tarifa = 50;
+        String[] datos = { "miguel", "jose", "20", matriculados.toString(), tarifa.toString() };
+        double ingresosAnteriores = club.ingresos();
+        club.anyadirActividad(grupo);
+        assertEquals(ingresosAnteriores + tarifa * matriculados, club.ingresos());
+    }
 
-        //Assert
-
+    @Test
+    @DisplayName("Añadir grupo con datos invalidos")
+    void anyadirGrupoConDatosInvalidos() throws ClubException{
+        String[] datos = { "miguel", "jose", "estoNoEsUnNumero", "estoTampoco", "estoAunMenos" };
+        assertThrows(ClubException.class, () -> club.anyadirActividad(datos));
+    }
+    @Test
+    @DisplayName("Las plazas libres son correctas")
+    void plazasLibresCorrectas() throws ClubException{
+        club.anyadirActividad(grupo);
+        int plazasLibres = grupo.plazasLibres();
+        String actividad = grupo.getActividad();
+        assertEquals(club.plazasLibres(actividad), plazasLibres);
+    }
+    @Test
+    @DisplayName("Matricular demasiadas personas")
+    void matricularDemasiasPersonas() throws ClubException{
+        club.anyadirActividad(grupo);
+        assertThrows(ClubException.class, () -> club.matricular(grupo.getActividad(), 99999));
+    }
+    @Test
+    @DisplayName("Matricular personas adecuadas")
+    void matricularPersonasAdecuadas() throws ClubException{
+        club.anyadirActividad(grupo);
+        int plazasLibres = grupo.plazasLibres();
+        String actividad = grupo.getActividad();
+        club.matricular(actividad, plazasLibres);
+        assertEquals(0, grupo.plazasLibres());
     }
 }
