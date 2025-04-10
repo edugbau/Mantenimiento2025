23d19c084fd4f0005355cbfc7a18d2504c93310f
edugbau <edugb@uma.es>
2025-04-10 11:23:33 +0200

    Update EvolutionaryAlgorithmTest.java

--- Practica4/src/test/java/org/mps/EvolutionaryAlgorithmTest.java
+++ Practica4/src/test/java/org/mps/EvolutionaryAlgorithmTest.java
@@ -1,5 +1,21 @@
 package org.mps;
 
-public class EvolutionaryAlgorithmTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.mps.crossover.TwoPointCrossover;
+import org.mps.mutation.GaussianMutation;
+import org.mps.selection.TournamentSelection;
 
+public class EvolutionaryAlgorithmTest {
+    EvolutionaryAlgorithm algorithm;
+
+    @BeforeEach
+    public void setUp() throws EvolutionaryAlgorithmException {
+        TournamentSelection tournamentSelection = new TournamentSelection(10);
+        GaussianMutation gaussianMutation = new GaussianMutation();
+        TwoPointCrossover twoPointCrossover = new TwoPointCrossover();
+
+        algorithm = new EvolutionaryAlgorithm(tournamentSelection, gaussianMutation, twoPointCrossover);
+    }
+
+    
 }
