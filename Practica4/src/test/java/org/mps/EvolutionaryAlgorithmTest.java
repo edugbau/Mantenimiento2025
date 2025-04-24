806ed87fc0bc39932d3d556b99e2bde6d25a31e6
edugbau <edugb@uma.es>
2025-04-24 11:02:16 +0200

    commit inicial practica 5

--- Practica4/src/test/java/org/mps/EvolutionaryAlgorithmTest.java
+++ Practica4/src/test/java/org/mps/EvolutionaryAlgorithmTest.java
@@ -1,5 +1,6 @@
 package org.mps;
 
+//Eduardo González Bautista y Juan Manuel Valenzuela González
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
