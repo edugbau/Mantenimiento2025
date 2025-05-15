b938353432d4c5c29a2fabef1fc4bba51e31439a
edugbau <edugb@uma.es>
2025-05-15 12:32:06 +0200

    paçente

--- Practica6/src/test/java/com/uma/example/springuma/integration/base/PacienteControllerIT.java
+++ Practica6/src/test/java/com/uma/example/springuma/integration/base/PacienteControllerIT.java
@@ -1,4 +1,78 @@
 package com.uma.example.springuma.integration.base;
 
-public class PacienteControllerIT {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uma.example.springuma.model.Medico;
+import com.uma.example.springuma.model.Paciente;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+public class PacienteControllerIT extends AbstractIntegration{
+
+    private Paciente paciente;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        paciente = new Paciente();
+        paciente.setNombre("Kinder Malo");
+        paciente.setEdad(17);
+        paciente.setDni("11111111A");
+
+        Medico medico = new Medico();
+        medico.setNombre("Pimp flaco");
+        medico.setDni("22222222B");
+
+        paciente.setMedico(medico);
+    }
+
+    @Test
+    @DisplayName("Crear un paciente y recibirlo correctamente")
+    public void crearPacienteYRecibirloCorrectamente() throws Exception {
+
+        //crear
+        this.mockMvc.perform(post("/paciente").
+                contentType("application/json").
+                content(objectMapper.writeValueAsString(paciente))).
+                andExpect(status().isCreated()).
+                andExpect(status().is2xxSuccessful());
+
+        //obtener
+        this.mockMvc.perform(get("/paciente/1"))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$[0]").value(paciente));
+
+        //actualizar
+        paciente.setNombre("Kinder Bueno");
+
+        this.mockMvc.perform(post("/paciente").
+                        contentType("application/json").
+                        content(objectMapper.writeValueAsString(paciente))).
+                andExpect(status().isCreated()).
+                andExpect(status().is2xxSuccessful());
+
+        //comprobar los cambios
+        this.mockMvc.perform(get("/paciente/1"))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$[0].nombre").value("Kinder Bueno"));
+    }
+
 }
